3af9fef8b62b72bfbf8965b42ddb37141e65f379
KomalR <30585552+KomalRangoonwala@users.noreply.github.com>
2018-08-23 11:59:57 +0530

    Update wpplugin-styles.php
    Delete scrutinizer bug fix

--- includes/wpplugin-styles.php
+++ includes/wpplugin-styles.php
@@ -2,7 +2,7 @@
 //conditionally loading CSS on plugin settings page only
 
 	function wpplugin_admin_styles($hook){
-		/** @scrutinizer ignore-call */ wp_register_style(
+		wp_register_style(
 			'wpplugin-admin',
 			WPPLUGIN_URL.'admin/css/wpplugin-admin-style.css',
 			[],
@@ -12,16 +12,15 @@
 	if('toplevel_page_wpplugin'==$hook 
 		OR 'wpplugin_page_wpplugin-feature-1'==$hook
 		){
-		/** @scrutinizer ignore-call */ 
+		
 		wp_enqueue_style('wpplugin-admin');
 	}
 }
-/** @scrutinizer ignore-call */ 
 add_action('admin_enqueue_scripts','wpplugin_admin_styles');
 
 //load CSS on frontend
-	function wpplugin_frontend_styles(/** @scrutinizer ignore-unused */ $hook){
-		/** @scrutinizer ignore-call */ 
+	function wpplugin_frontend_styles( $hook){
+		
 		wp_register_style(
 			'wpplugin-frontend',
 			WPPLUGIN_URL.'frontend/css/wpplugin-frontend-style.css',
@@ -29,9 +28,8 @@ add_action('admin_enqueue_scripts','wpplugin_admin_styles');
 			time()
 		);
 	
-	if(/** @scrutinizer ignore-call */ is_single()){
-		/** @scrutinizer ignore-call */ wp_enqueue_style('wpplugin-frontend');
+	if(is_single()){
+		wp_enqueue_style('wpplugin-frontend');
 	}
 }
-/** @scrutinizer ignore-call */ 
 add_action('wp_enqueue_scripts','wpplugin_frontend_styles',100);
