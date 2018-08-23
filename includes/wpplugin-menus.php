2b45444b4861ef375a9cd4eda30f58d8bee7b8d7
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-08-23 08:02:46 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- includes/wpplugin-menus.php
+++ includes/wpplugin-menus.php
@@ -2,8 +2,8 @@
 function wpplugin_settings_page()
 {
 	/** @scrutinizer ignore-call */ add_menu_page(
-	/** @scrutinizer ignore-call */ __('Plugin Name','wpplugin'),
-		/** @scrutinizer ignore-call */ __('Plugin Menu','wpplugin'),
+	/** @scrutinizer ignore-call */ __('Plugin Name', 'wpplugin'),
+		/** @scrutinizer ignore-call */ __('Plugin Menu', 'wpplugin'),
 		'manage_options',
 		'wpplugin',
 		'wpplugin_settings_page_markup',
@@ -12,32 +12,32 @@ function wpplugin_settings_page()
 	);
 }
 /** @scrutinizer ignore-call */ 
-add_action('admin_menu','wpplugin_settings_page');
+add_action('admin_menu', 'wpplugin_settings_page');
 function wpplugin_settings_page_markup()
 {
-	if(! /** @scrutinizer ignore-call */ current_user_can('manage_options')){
+	if (!/** @scrutinizer ignore-call */ current_user_can('manage_options')) {
 		return;
 	}
 	include(WPPLUGIN_DIR.'templates/admin/settings-page.php');
 }
 
-function wpplugin_default_subpage(){
+function wpplugin_default_subpage() {
 	/** @scrutinizer ignore-call */ add_dashboard_page(
-		/** @scrutinizer ignore-call */ __('Cool default sub page','wpplugin'),
-		/** @scrutinizer ignore-call */ __('Custom sub page','wpplugin'),
+		/** @scrutinizer ignore-call */ __('Cool default sub page', 'wpplugin'),
+		/** @scrutinizer ignore-call */ __('Custom sub page', 'wpplugin'),
 		'manage_options',
 		'wpplugin-subpage',
 		'wpplugin_settings_page_markup'
 	);
 }
 /** @scrutinizer ignore-call */ 
-add_action('admin_menu','wpplugin_default_subpage');
+add_action('admin_menu', 'wpplugin_default_subpage');
 //Add a link to your settings page in your plugin
-function wpplugin_add_settings_link($links){
-	$settings_link='<a href="admin.php?page=wpplugin">'. /** @scrutinizer ignore-call */ __('Settings','wpplugin').'</a>';
-	array_push($links,$settings_link); 
+function wpplugin_add_settings_link($links) {
+	$settings_link='<a href="admin.php?page=wpplugin">'./** @scrutinizer ignore-call */ __('Settings', 'wpplugin').'</a>';
+	array_push($links, $settings_link); 
 	return $links;
 }
-$filter_name="plugin_action_links_". /** @scrutinizer ignore-call */ plugin_basename(__FILE__);
+$filter_name="plugin_action_links_"./** @scrutinizer ignore-call */ plugin_basename(__FILE__);
 /** @scrutinizer ignore-call */ 
-add_filter($filter_name,'wpplugin_add_settings_link');
+add_filter($filter_name, 'wpplugin_add_settings_link');
--- includes/wpplugin-settings-fields.php
+++ includes/wpplugin-settings-fields.php
@@ -1,7 +1,7 @@
   <?php
 
-	function wpplugin_settings(){
-		if(! /** @scrutinizer ignore-call */ get_option('wpplugin_settings')){
+	function wpplugin_settings() {
+		if (!/** @scrutinizer ignore-call */ get_option('wpplugin_settings')) {
 			 /** @scrutinizer ignore-call */ add_option('wpplugin_settings');
 		}
 	
@@ -11,7 +11,7 @@
 			//unique id for the section
 			'wpplugin_settings_section',
 			//section title
-			 /** @scrutinizer ignore-call */ __('Plugin settings section','wpplugin'),
+			 /** @scrutinizer ignore-call */ __('Plugin settings section', 'wpplugin'),
 			//callback for an optional description
 			'wpplugin_settings_section_callback',
 			//admin page to add section to
@@ -33,14 +33,14 @@
 
 	}
 	/** @scrutinizer ignore-call */ 
-	add_action('add_meta_boxes','cd_meta_box_add');
+	add_action('add_meta_boxes', 'cd_meta_box_add');
 	/** @scrutinizer ignore-call */ 
-	add_action('admin_init','wpplugin_settings');
+	add_action('admin_init', 'wpplugin_settings');
 	
-	function wpplugin_settings_section_callback(){
-		/** @scrutinizer ignore-call */ esc_html_e('Plugin settings section description','wpplugin');
+	function wpplugin_settings_section_callback() {
+		/** @scrutinizer ignore-call */ esc_html_e('Plugin settings section description', 'wpplugin');
 	}
 	function cd_meta_box_cb()
 	{
-    	echo 'What you put here, show\'s up in the meta box';   
+		echo 'What you put here, show\'s up in the meta box';   
 	}
