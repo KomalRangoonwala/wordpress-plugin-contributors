d9619daa5c3c3d8b90065d102f9fe3d2281e29ba
KomalR <30585552+KomalRangoonwala@users.noreply.github.com>
2018-08-23 12:17:52 +0530

    Update wpplugin-settings-fields.php
    Scrutinizer bug fix

--- includes/wpplugin-settings-fields.php
+++ includes/wpplugin-settings-fields.php
@@ -1,44 +1,45 @@
   <?php
 
 	function wpplugin_settings(){
-		if(! get_option('wpplugin_settings')){
-			 add_option('wpplugin_settings');
+		if(! /** @scrutinizer ignore-call */ get_option('wpplugin_settings')){
+			 /** @scrutinizer ignore-call */ add_option('wpplugin_settings');
 		}
 	
 		//define at least one section for our fields
+		/** @scrutinizer ignore-call */ 
 		add_settings_section(
 			//unique id for the section
 			'wpplugin_settings_section',
 			//section title
-			 __('Plugin settings section','wpplugin'),
+			 /** @scrutinizer ignore-call */ __('Plugin settings section','wpplugin'),
 			//callback for an optional description
 			'wpplugin_settings_section_callback',
 			//admin page to add section to
 			'wpplugin'
 		);
 	
-		add_meta_box( 
+		/** @scrutinizer ignore-call */ add_meta_box( 
 			'my-meta-box-id', 
 			'My First Meta Box', 
 			'cd_meta_box_cb', 
 			'post', 
 			'normal', 
 			'high' );
+		/** @scrutinizer ignore-call */ 
 		register_setting(
 			'wpplugin_settings',
 			'wpplugin_settings'
 		);
 
 	}
+	/** @scrutinizer ignore-call */ 
 	add_action('add_meta_boxes','cd_meta_box_add');
 	add_action('admin_init','wpplugin_settings');
 	
 	function wpplugin_settings_section_callback(){
-		esc_html_e('Plugin settings section description','wpplugin');
+		/** @scrutinizer ignore-call */ esc_html_e('Plugin settings section description','wpplugin');
 	}
 	function cd_meta_box_cb()
 	{
     	echo 'What you put here, show\'s up in the meta box';   
 	}
-
-?>
