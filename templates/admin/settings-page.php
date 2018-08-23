c1b668346920753e46975636a53ebc4aa46cde20
KomalR <30585552+KomalRangoonwala@users.noreply.github.com>
2018-08-23 11:57:26 +0530

    Update settings-page.php
    Delete scrutinizer bug fix

--- templates/admin/settings-page.php
+++ templates/admin/settings-page.php
@@ -1,13 +1,13 @@
 <div class="wrap">
-	<h1><?php /** @scrutinizer ignore-call */ esc_html_e(/** @scrutinizer ignore-call */ get_admin_page_title()); ?></h1>
+	<h1><?php esc_html_e(get_admin_page_title()); ?></h1>
 
 	  <form method="post" action="options.php">
 
 	<!-- Display necessary hidden fields for settings -->
-    <?php /** @scrutinizer ignore-call */ settings_fields( 'wpplugin_settings' ); ?>
+    <?php settings_fields( 'wpplugin_settings' ); ?>
     <!-- Display the settings sections for the page -->
-    <?php /** @scrutinizer ignore-call */ do_settings_sections( 'wpplugin' ); ?>
+    <?php do_settings_sections( 'wpplugin' ); ?>
     <!-- Default Submit Button -->
-    <?php /** @scrutinizer ignore-call */ submit_button(); ?>
+    <?php submit_button(); ?>
   </form>
 </div>
