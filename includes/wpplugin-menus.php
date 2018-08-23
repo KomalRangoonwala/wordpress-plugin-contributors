a918e5785a5058eed06379600f81d988f6193526
KomalR <30585552+KomalRangoonwala@users.noreply.github.com>
2018-08-23 11:21:49 +0530

    Update wpplugin-menus.php
    Scrutinizer bug resolution

--- includes/wpplugin-menus.php
+++ includes/wpplugin-menus.php
@@ -1,8 +1,8 @@
 <?php
 function wpplugin_settings_page()
 {
-	add_menu_page(
-		__('Plugin Name','wpplugin'),
+	/** @scrutinizer ignore-call */ add_menu_page(
+	/** @scrutinizer ignore-call */	__('Plugin Name','wpplugin'),
 		__('Plugin Menu','wpplugin'),
 		'manage_options',
 		'wpplugin',
@@ -11,18 +11,18 @@ function wpplugin_settings_page()
 		100
 	);
 }
-add_action('admin_menu','wpplugin_settings_page');
+/** @scrutinizer ignore-call */ add_action('admin_menu','wpplugin_settings_page');
 function wpplugin_settings_page_markup()
 {
-	if(!current_user_can('manage_options')){
+	if(!/** @scrutinizer ignore-call */ current_user_can('manage_options')){
 		return;
 	}
 	include(WPPLUGIN_DIR.'templates/admin/settings-page.php');
 }
 
 function wpplugin_default_subpage(){
-	add_dashboard_page(
-		__('Cool default sub page','wpplugin'),
+	/** @scrutinizer ignore-call */ add_dashboard_page(
+		/** @scrutinizer ignore-call */ __('Cool default sub page','wpplugin'),
 		__('Custom sub page','wpplugin'),
 		'manage_options',
 		'wpplugin-subpage',
@@ -32,9 +32,10 @@ function wpplugin_default_subpage(){
 add_action('admin_menu','wpplugin_default_subpage');
 //Add a link to your settings page in your plugin
 function wpplugin_add_settings_link($links){
-	$settings_link='<a href="admin.php?page=wpplugin">'.__('Settings','wpplugin').'</a>';
+	$settings_link='<a href="admin.php?page=wpplugin">'./** @scrutinizer ignore-call */ __('Settings','wpplugin').'</a>';
 	array_push($links,$settings_link); 
 	return $links;
 }
-$filter_name="plugin_action_links_".plugin_basename(__FILE__);
+$filter_name="plugin_action_links_"./** @scrutinizer ignore-call */ plugin_basename(__FILE__);
+/** @scrutinizer ignore-call */ 
 add_filter($filter_name,'wpplugin_add_settings_link');
