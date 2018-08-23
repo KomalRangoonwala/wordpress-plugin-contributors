4602035da9d1a488521947380ca8c8f51eb70eda
KomalR <30585552+KomalRangoonwala@users.noreply.github.com>
2018-08-23 13:06:31 +0530

    Update wpplugin-styles.php
    Defining path on top

--- includes/wpplugin-styles.php
+++ includes/wpplugin-styles.php
@@ -1,5 +1,8 @@
 <?php
 //conditionally loading CSS on plugin settings page only
+define('WPPLUGIN_URL',plugin_dir_url(__FILE__));
+define( 'WPPLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define('WPPLUGIN_UPLOADS_URL',wp_upload_dir(__FILE__));
 
 	function wpplugin_admin_styles($hook){
 		/** @scrutinizer ignore-call */ wp_register_style(
