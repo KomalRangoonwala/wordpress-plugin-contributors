7bd865c168104609abd055358cf2c567ccf2d9c3
KomalR <30585552+KomalRangoonwala@users.noreply.github.com>
2018-08-23 13:04:07 +0530

    Update settings-page.php
    Scrutinizer bug fix

--- templates/admin/settings-page.php
+++ templates/admin/settings-page.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-	<h1><?php /** @scrutinizer ignore-call */ esc_html_e(get_admin_page_title()); ?></h1>
+	<h1><?php /** @scrutinizer ignore-call */ esc_html_e(/** @scrutinizer ignore-call */ get_admin_page_title()); ?></h1>
 
 	  <form method="post" action="options.php">
 
