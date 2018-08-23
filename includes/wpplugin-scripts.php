2d131b26e887e61f9eddf94e6e8a53b354ca4b8b
KomalR <30585552+KomalRangoonwala@users.noreply.github.com>
2018-08-23 12:19:49 +0530

    Update wpplugin-scripts.php
    Scrutinizer bug fix

--- includes/wpplugin-scripts.php
+++ includes/wpplugin-scripts.php
@@ -1,29 +1,31 @@
 <?php
 //Conditionally load JS on plugin settings page only
 function wpplugin_admin_scripts($hook){
-	 wp_register_script(
+	 /** @scrutinizer ignore-call */ wp_register_script(
 		'wpplugin-admin',
 		WPPLUGIN_URL.'admin/js/wpplugin-admin.js',
 		['jquery'],
 		time()
 	);
-	 wp_localize_script('wpplugin-admin','wpplugin',[
+	 /** @scrutinizer ignore-call */ wp_localize_script('wpplugin-admin','wpplugin',[
 		'hook'=>$hook
 	]);
 	if('toplevel_page_wpplugin'==$hook){
-		wp_enqueue_script('wpplugin-admin');
+		/** @scrutinizer ignore-call */ wp_enqueue_script('wpplugin-admin');
 	}
 }
+/** @scrutinizer ignore-call */ 
 add_action('admin_enqueue_scripts','wpplugin_admin_scripts');
 //Conditionally load JS on single post pages only on frontend
 function wpplugin_frontend_scripts(){
-	wp_register_script(
+	/** @scrutinizer ignore-call */ wp_register_script(
 		'wpplugin-frontend',
 		WPPLUGIN_URL.'frontend/js/wpplugin-frontend.js',
 		[],
 		time()
 	);
-	if(is_single()){
+	if(/** @scrutinizer ignore-call */ is_single()){
+		/** @scrutinizer ignore-call */ 
 		wp_enqueue_script('wpplugin-frontend');
 	}
 }
