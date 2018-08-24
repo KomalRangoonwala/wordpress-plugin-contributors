a75951313eebdbec836ae0b97b35a8e047fedecb
KomalR <30585552+KomalRangoonwala@users.noreply.github.com>
2018-08-24 18:50:10 +0530

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,9 +13,6 @@ if ( ! $_tests_dir ) {
 
 
 
-// Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
-
 /**
  * Manually load the plugin being tested.
  */
