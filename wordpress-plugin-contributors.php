755353ab64d8c56de96af38d7c6b7e248063dac5
KomalR <30585552+KomalRangoonwala@users.noreply.github.com>
2018-08-31 22:22:52 +0530

    Update wordpress-plugin-contributors.php
    Added features as asked

--- wordpress-plugin-contributors.php
+++ wordpress-plugin-contributors.php
@@ -14,15 +14,16 @@ if (!defined('WPINC'))
 {
 	die;
 }
-define('WPPLUGIN_URL', /** @scrutinizer ignore-call */ plugin_dir_url(__FILE__));
-define('WPPLUGIN_DIR', /** @scrutinizer ignore-call */ plugin_dir_path(__FILE__));
-define('WPPLUGIN_UPLOADS_URL', /** @scrutinizer ignore-call */ wp_upload_dir(__FILE__));
+define('WPPLUGIN_URL', plugin_dir_url(__FILE__));
+define('WPPLUGIN_DIR', plugin_dir_path(__FILE__));
+define('WPPLUGIN_UPLOADS_URL', wp_upload_dir(__FILE__));
+
 include(plugin_dir_path(__FILE__).'includes/wpplugin-styles.php');
+
 function cd_meta_box_add()
 {
 	$multi_posts=array('post', 'page');
 	foreach ($multi_posts as $multi_post) {
-	/** @scrutinizer ignore-call */ 
 		add_meta_box(
 				'my-meta-box-id', //id
 	 			'Contributors', //title
@@ -34,91 +35,162 @@ function cd_meta_box_add()
 
 	}
 }
-/** @scrutinizer ignore-call */ 
 
 add_action('add_meta_boxes', 'cd_meta_box_add');
 //display HTML data
 
-function cd_meta_box_cb(/** @scrutinizer ignore-unused */ $post)
+function cd_meta_box_cb($post)
 {
 	global $post;
  	echo'<b> Select the contributors that have contributed to this post: </b>';
  	echo '<br><br>';
-	/** @scrutinizer ignore-call */ 
  	wp_nonce_field('my_meta_box_nonce', 'meta_box_nonce');
 	global $wpdb;
-	$authors=$wpdb->get_results("SELECT ID, user_nicename from $wpdb->users ORDER BY user_nicename");
+
+	$authors=$wpdb->get_results("SELECT wp_users.ID, wp_users.user_nicename 
+	FROM wp_users INNER JOIN wp_usermeta 
+	ON wp_users.ID = wp_usermeta.user_id 
+	WHERE wp_usermeta.meta_key = 'wp_capabilities' 
+	AND wp_usermeta.meta_value LIKE '%author%' OR wp_usermeta.meta_value LIKE '%contributor%' OR wp_usermeta.meta_value LIKE '%editor%' OR wp_usermeta.meta_value LIKE '%administrator%'   
+	ORDER BY wp_users.user_nicename");
+
+	$current_user = wp_get_current_user();
 	foreach ($authors as $author) {
-		echo"<input type='checkbox' id='my_meta_box_check' name='my_meta_box_check[]'";
-		echo"value=";
-		/** @scrutinizer ignore-call */ 
-		the_author_meta('user_nicename', $author->ID);
-		echo">";
-		echo"<label for='author'>";
-		/** @scrutinizer ignore-call */ 
+		$author_info=get_userdata($author->ID);
+		//$author_role=$author_info->roles;
+		$author_first_name=$author_info->first_name;
+		$author_last_name=$author_info->last_name;
+		if(strcmp($current_user->user_nicename,$author->user_nicename)==0)
+		{		
+			echo"<input type='checkbox' id='my_meta_box_check' name='my_meta_box_check[]'";
+			echo"value=";
+			the_author_meta('user_nicename', $author->ID);
+			echo" checked disabled>";
+
+			echo"<input type='hidden' id='my_meta_box_check' name='my_meta_box_check[]'";
+			echo"value=";
+			the_author_meta('user_nicename', $author->ID);
+			echo">";
+		}
+		else
+		{
+			echo"<input type='checkbox' id='my_meta_box_check' name='my_meta_box_check[]'";
+			echo"value=";
+			the_author_meta('user_nicename', $author->ID);
+			echo">";	
+		}
+		echo $author_first_name ." ". $author_last_name ." ";
+		echo"(";
+		echo"<label id='labelid' for='author'>";
 		the_author_meta('user_nicename', $author->ID);
 		echo"</label>";
+		echo")";
 		echo "<br />";
 	}
 }
 //save custom data when our post is saved
 function save_custom_data($post_id)
 {
-	global $post;
-	$contributor=/** @scrutinizer ignore-call */ get_post_meta($post->ID, 'my_meta_box_check', true);
+	global $post,$wpdb;
+
 	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
-	if (!isset($_POST['meta_box_nonce']) || !/** @scrutinizer ignore-call */ wp_verify_nonce($_POST['meta_box_nonce'], 'my_meta_box_nonce')) return;
-	if (!/** @scrutinizer ignore-call */ current_user_can('edit_post')) return;
+	if (!isset($_POST['meta_box_nonce']) || !wp_verify_nonce($_POST['meta_box_nonce'], 'my_meta_box_nonce')) return;
+	if (!current_user_can('edit_post')) return;
 	if (isset($_POST['my_meta_box_check'])) 
 	{
-				/** @scrutinizer ignore-call */ 
 		update_post_meta($post_id, 'my_meta_box_check', $_POST['my_meta_box_check']);
+	//	$tablename = $wpdb->prefix.'authorlist';
+	//	$wpdb->insert($tablename,array('authorname'=>$post_id,'authorpost'=>$contributor));
+
 	}
   	else 
   	{
-				/** @scrutinizer ignore-call */ 
 		delete_post_meta($post_id, 'my_meta_box_check');
 	}
 }
-/** @scrutinizer ignore-call */ 
+
 add_action('save_post', 'save_custom_data');
 
 function displaymeta($content) {
 			global $post;
-			$m_meta_description=/** @scrutinizer ignore-call */ get_post_meta($post->ID, 'my_meta_box_check', true);
+			$m_meta_description=get_post_meta($post->ID, 'my_meta_box_check', true);
 			global $wpdb;
-			$user_nicenames=$wpdb->get_results("SELECT id,user_nicename,user_email,user_url FROM 
+			$user_nicenames=$wpdb->get_results("SELECT id,user_nicename,user_email,user_url,display_name FROM 
 			{$wpdb->prefix}users", ARRAY_N);	
-			
-			if ($m_meta_description!=NULL) {
-			
+
+
+			if ($m_meta_description!=NULL) 
+			{
+
 			$content.="<div id='cont'>";
 			$content.="<b>"."Contributors"."</b><br>";
 			foreach ($user_nicenames as $nice_name) 
 			{
+
 				$user_id=$nice_name[0];
 				$name=$nice_name[1];
 				$user_email=$nice_name[2];
 				$user_website=$nice_name[3];
-				$author_page=/** @scrutinizer ignore-call */ get_author_posts_url($user_id);
+				$display_name=$nice_name[4];
+				$author_page=get_author_posts_url($user_id);
+
+				$user_info=get_userdata($user_id);
+				$first_name=$user_info->first_name;
+				$last_name=$user_info->last_name;
+
 
 				foreach ($m_meta_description as $val)
 				{
 					$flag=strcmp($name, $val);
 					if ($flag==0)
 					{
-						$content.=/** @scrutinizer ignore-call */ get_avatar($user_id, 50);
+						$content.=get_avatar($user_id, 50);
 						$content.="<a href=".$author_page.">";
-						$content.=$name."</a><br>";
+						
+						if($first_name!='' AND $last_name!='')
+						{
+							$content.=$first_name." ".$last_name ."</a><br>";
+						}
+						else
+						{
+							$content.=$display_name."</a><br>";
+						}
 					}
 				}
 			}
+
 			$content.="</div>";
-		} else
+
+
+		} 
+		else
 		{
 			$content="<b> [Message from Contributors plugin] </b> Please mention contributors in post!";
 		}
 		return $content;
 }
-/** @scrutinizer ignore-call */ 
+
 add_filter('the_content', 'displaymeta');
+
+add_action( 'pre_get_posts', 'modify_author_query' );
+
+function modify_author_query( $query ) {
+    // Check if on frontend and author query is modified
+    if ( ! is_admin() && $query->is_author() ) {
+        $author_name =  $query->query_vars['author_name'];
+        //$userdata = get_user_by('slug',$author_name);
+        //$userid = $user->ID;
+
+        $meta_query = array(  
+            array(
+                'key' => 'my_meta_box_check',
+                'value' => $author_name,
+                'compare' => 'LIKE'
+            )
+        );
+        $query->set( 'meta_query', $meta_query );
+
+        // unset the default author
+        unset( $query->query_vars['author_name'] );
+    }
+}
