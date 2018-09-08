a58cdd80a0e96226a1e4e2bdc5ac01fd7fd5a1d2
KomalR <30585552+KomalRangoonwala@users.noreply.github.com>
2018-09-08 17:46:32 +0530

    Update wpplugin-settings-fields.php

--- includes/wpplugin-settings-fields.php
+++ includes/wpplugin-settings-fields.php
@@ -1,47 +1,65 @@
-  <?php
+<?php
 
-	function wpplugin_settings() {
-		if (!/** @scrutinizer ignore-call */ get_option('wpplugin_settings')) {
-			 /** @scrutinizer ignore-call */ add_option('wpplugin_settings');
-		}
-	
-		//define at least one section for our fields
-		/** @scrutinizer ignore-call */ 
-		add_settings_section(
-			//unique id for the section
-			'wpplugin_settings_section',
-			//section title
-			 /** @scrutinizer ignore-call */ __('Plugin settings section', 'wpplugin'),
-			//callback for an optional description
-			'wpplugin_settings_section_callback',
-			//admin page to add section to
-			'wpplugin'
-		);
-	
-		/** @scrutinizer ignore-call */ add_meta_box( 
-			'my-meta-box-id', 
-			'My First Meta Box', 
-			'cd_meta_box_cb', 
-			'post', 
-			'normal', 
-			'high' );
-		/** @scrutinizer ignore-call */ 
-		register_setting(
-			'wpplugin_settings',
-			'wpplugin_settings'
-		);
+function wpplugin_settings() {
 
-	}
-	/** @scrutinizer ignore-call */ 
-	add_action('add_meta_boxes', 'cd_meta_box_add');
-	/** @scrutinizer ignore-call */ 
-	add_action('admin_init', 'wpplugin_settings');
-	
-	function wpplugin_settings_section_callback() {
-		/** @scrutinizer ignore-call */ esc_html_e('Plugin settings section description', 'wpplugin');
-	}
-	function cd_meta_box_cb()
-	{
-		echo 'What you put here, show\'s up in the meta box';   
-	}
-?>
+  // If plugin settings don't exist, then create them
+  if( !get_option( 'wpplugin_settings' ) ) {
+      add_option( 'wpplugin_settings' );
+  }
+
+  // Define (at least) one section for our fields
+  add_settings_section(
+    // Unique identifier for the section
+    'wpplugin_settings_section',
+    // Section Title
+    __( 'Settings Section', 'wpplugin' ),
+    // Callback for an optional description
+    'wpplugin_settings_section_callback',
+    // Admin page to add section to
+    'wpplugin'
+  );
+
+   // Checkbox Field
+  add_settings_field(
+    'wpplugin_settings_checkbox',
+    __( 'Check to enable the Contributors meta-box', 'wpplugin'),
+    'wpplugin_settings_checkbox_callback',
+    'wpplugin',
+    'wpplugin_settings_section',
+    [
+      'label1' => 'Post and pages',
+    ]
+  );
+
+
+
+  register_setting(
+    'wpplugin_settings',
+    'wpplugin_settings'
+  );
+
+}
+add_action( 'admin_init', 'wpplugin_settings' );
+
+function wpplugin_settings_section_callback() {
+
+  esc_html_e( 'You can control whether the Contributors meta-box should be displayed or not.', 'wpplugin' );
+
+}
+
+function wpplugin_settings_checkbox_callback( $args ) {
+
+  $options = get_option( 'wpplugin_settings' );
+
+  $checkbox = '';
+  if( isset( $options[ 'checkbox' ] ) ) {
+    $checkbox = esc_html( $options['checkbox'] );
+  }
+
+  $html = '<input type="checkbox" id="wpplugin_settings_checkbox" name="wpplugin_settings[checkbox]" value="1"' . checked( 1, $checkbox, false ) . '/>';
+  $html .= '&nbsp;';
+  $html .= '<label for="wpplugin_settings_checkbox">' . $args['label1'] . '</label>';
+
+  echo $html;
+
+}
