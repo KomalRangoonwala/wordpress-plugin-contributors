18fa9f7dbf90b1e6850fa60a657fb7477e99dc6c
KomalR <30585552+KomalRangoonwala@users.noreply.github.com>
2018-09-08 17:45:43 +0530

    Update wpplugin-menus.php
    Added a menu page and settings field to enable and disable custom meta-box.

--- includes/wpplugin-menus.php
+++ includes/wpplugin-menus.php
@@ -1,43 +1,34 @@
 <?php
-function wpplugin_settings_page()
-{
-	/** @scrutinizer ignore-call */ add_menu_page(
-	/** @scrutinizer ignore-call */ __('Plugin Name', 'wpplugin'),
-		/** @scrutinizer ignore-call */ __('Plugin Menu', 'wpplugin'),
-		'manage_options',
-		'wpplugin',
-		'wpplugin_settings_page_markup',
-		'dashicons-image-filter',
-		100
-	);
-}
-/** @scrutinizer ignore-call */ 
-add_action('admin_menu', 'wpplugin_settings_page');
+
 function wpplugin_settings_page_markup()
 {
-	if (!/** @scrutinizer ignore-call */ current_user_can('manage_options')) {
-		return;
-	}
-	include(WPPLUGIN_DIR.'templates/admin/settings-page.php');
+  // Double check user capabilities
+  if ( !current_user_can('manage_options') && !is_admin()) {
+      return;
+  }
+  include( WPPLUGIN_DIR . 'templates/admin/settings-page.php');
 }
 
-function wpplugin_default_subpage() {
-	/** @scrutinizer ignore-call */ add_dashboard_page(
-		/** @scrutinizer ignore-call */ __('Cool default sub page', 'wpplugin'),
-		/** @scrutinizer ignore-call */ __('Custom sub page', 'wpplugin'),
-		'manage_options',
-		'wpplugin-subpage',
-		'wpplugin_settings_page_markup'
-	);
+function wpplugin_settings_pages()
+{
+  add_menu_page(
+    __( 'Contributors Plugin', 'wpplugin' ),
+    __( 'Contributors Plugin', 'wpplugin' ),
+    'manage_options',
+    'wpplugin',
+    'wpplugin_settings_page_markup',
+    'dashicons-nametag',
+    100
+  );
+
 }
-/** @scrutinizer ignore-call */ 
-add_action('admin_menu', 'wpplugin_default_subpage');
-//Add a link to your settings page in your plugin
-function wpplugin_add_settings_link($links) {
-	$settings_link='<a href="admin.php?page=wpplugin">'./** @scrutinizer ignore-call */ __('Settings', 'wpplugin').'</a>';
-	array_push($links, $settings_link); 
-	return $links;
+add_action( 'admin_menu', 'wpplugin_settings_pages' );
+
+// Add a link to your settings page in your plugin
+function wpplugin_add_settings_link( $links ) {
+    $settings_link = '<a href="admin.php?page=wpplugin">' . __( 'Settings' ) . '</a>';
+    array_push( $links, $settings_link );
+    return $links;
 }
-$filter_name="plugin_action_links_"./** @scrutinizer ignore-call */ plugin_basename(__FILE__);
-/** @scrutinizer ignore-call */ 
-add_filter($filter_name, 'wpplugin_add_settings_link');
+$filter_name = "plugin_action_links_" . plugin_basename( __FILE__ );
+add_filter( $filter_name, 'wpplugin_add_settings_link' );
